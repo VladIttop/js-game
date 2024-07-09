0520137594f890c912ecc99c63136e3f0a16dae2
VladIttop <129985238+VladIttop@users.noreply.github.com>
2024-07-09 20:42:16 +0100

    commit

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,21 @@
 const clickText = document.querySelector('.clickText');
 const dropMenu = document.querySelector('.dropdown-menu');
-document.addEventListener('DOMContentLoaded', () => {
-  clickText.addEventListener('click', () => {
-    dropMenu.classList.toggle('is-open');
-  });
+clickText.addEventListener('click', () => {
+  dropMenu.classList.toggle('is-open');
 });
 
 const btn = document.querySelector('.header-right__btn');
 const btnDiv = document.querySelector('.header-right-btn-div');
-const img = document.querySelector('.header-right-btn-div__img');
+const img1 = document.querySelector('.header-right-btn-div__img.img1');
+const img2 = document.querySelector('.header-right-btn-div__img.img2');
+
 btn.addEventListener('click', () => {
+  if (img1.style.display !== 'none') {
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+  } else {
+    img1.style.display = 'block';
+    img2.style.display = 'none';
+  }
   btnDiv.classList.toggle('clicked');
 });
