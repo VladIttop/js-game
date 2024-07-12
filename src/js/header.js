fbe5854658ad142728024a4931e000bd8259b4d2
VladIttop <129985238+VladIttop@users.noreply.github.com>
2024-07-12 19:41:57 +0100

    dark theme

--- src/js/header.js
+++ src/js/header.js
@@ -1,16 +1,15 @@
 const clickText = document.querySelector('.clickText');
 const dropMenu = document.querySelector('.dropdown-menu');
-const arrow = document.querySelector(".drop-menu__img")
+const arrow = document.querySelector('.drop-menu__img');
 clickText.addEventListener('click', () => {
   dropMenu.classList.toggle('is-open');
-  arrow.classList.toggle("rotated")
+  arrow.classList.toggle('rotated');
 });
 
 const btn = document.querySelector('.header-right__btn');
 const btnDiv = document.querySelector('.header-right-btn-div');
 const img1 = document.querySelector('.header-right-btn-div__img.img1');
 const img2 = document.querySelector('.header-right-btn-div__img.img2');
-
 btn.addEventListener('click', () => {
   if (img1.style.display !== 'none') {
     img1.style.display = 'none';
@@ -20,4 +19,5 @@ btn.addEventListener('click', () => {
     img2.style.display = 'none';
   }
   btnDiv.classList.toggle('clicked');
+  document.documentElement.classList.toggle('dark');
 });
