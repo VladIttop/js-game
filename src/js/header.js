97ed1eec9b19d682d69b3e188eee2dd5ea84f6a1
VladIttop <129985238+VladIttop@users.noreply.github.com>
2024-07-15 18:39:16 +0100

    template

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,81 @@
+const calculator = document.getElementById('calculator');
+
+const games = [
+  // {
+  //   id: document.querySelector('#year-check'),
+  //   name: 'Високосний калькулятор',
+  //   category: 'numerical',
+  // },
+  // {
+  //   id: document.getElementById('guess-number'),
+  //   name: 'Вгадай число',
+  //   category: 'numerical',
+  // },
+  // {
+  //   id: rockPaper,
+  //   name: 'Камінь-Ножиці-Папір',
+  //   category: 'game',
+  // },
+  {
+    id: document.getElementById('calculator'),
+    name: 'Калькулятор',
+    category: 'numerical',
+  },
+  //{
+  //   id: document.getElementById('time-calculator'),
+  //   name: 'Калькулятор часу',
+  //   category: 'numerical',
+  // },
+  // {
+  //   id: 6,
+  //   name: 'Google динозаврик',
+  //   category: 'game',
+  // },
+  // {
+  //   id: 7,
+  //   name: 'Футбол',
+  //   category: 'game',
+  // },
+  // {
+  //   id: document.getElementById('biggest-number'),
+  //   name: 'Найбільше число',
+  //   category: 'numerical',
+  // },
+  // {
+  //   id: 9,
+  //   name: 'Наша команда',
+  //   category: 'acquaintance',
+  // },
+  // {
+  //   id: 10,
+  //   name: 'Вчений',
+  //   category: 'acquaintance',
+  // },
+];
+
+const allSections = document.querySelectorAll('section');
+const numerical = document.getElementById('numerical');
+const numericalGames = games.filter(game => game.category === 'numerical');
+console.log(numericalGames);
+const game = document.getElementById('game');
+const gameGames = games.filter(game => game.category === 'game');
+
+const acquaintance = document.getElementById('acquaintance');
+const acquaintanceGames = games.filter(
+  game => game.category === 'acquaintance'
+);
+
+numerical.addEventListener('click', () => {
+  allSections.forEach(section => {
+    section.style.display = 'none';
+  });
+  numericalGames.forEach(game => {
+    game.id.style.display = 'block';
+  });
+});
+
+console.log(games.id);
+
 const clickText = document.querySelector('.clickText');
 const dropMenu = document.querySelector('.dropdown-menu');
 const arrow = document.querySelector('.drop-menu__img');
@@ -10,12 +88,12 @@ const btn = document.querySelector('.header-right__btn');
 const btnDiv = document.querySelector('.header-right-btn-div');
 const img1 = document.querySelector('.header-right-btn-div__img.img1');
 const img2 = document.querySelector('.header-right-btn-div__img.img2');
-const link = document.querySelectorAll("a");
+const link = document.querySelectorAll('a');
 link.forEach(link => {
-  link.addEventListener("click" , (event) => {
+  link.addEventListener('click', event => {
     event.preventDefault();
-  })
-})
+  });
+});
 btn.addEventListener('click', () => {
   if (img1.style.display !== 'none') {
     img1.style.display = 'none';
