e82fcdf8f1d58b6dde1e04e4481492b65ddb6f38
ksva11ly <126807129+rayytsx@users.noreply.github.com>
2024-07-16 21:34:40 +0300

    4

--- src/js/time-calculator.js
+++ src/js/time-calculator.js
@@ -1,18 +1,23 @@
-function minutesToTime(minutes) {
-    if (isNaN(minutes) || minutes < 0) {
-      return "Invalid input";
-    }
-  
-    const hours = Math.floor(minutes / 60);
-    const remainingMinutes = minutes % 60;
-    return `${hours.toString().padStart(2, '0')} годин ${remainingMinutes.toString().padStart(2, '0')} хв.`;
+// const resultElement = document.querySelector(".calculator__time")
+const inputElement = document.querySelector(".calculator__wrapper--input")
+function minutesToTime() {
+  let minutes = inputElement.value;
+  console.log(minutes)
+  if (isNaN(minutes) || minutes < 0) {
+    return "Invalid input";
   }
-  
-  const minutes = parseInt(prompt("Введіть число хвилин: "));
-  if (isNaN(minutes)) {
-    console.log("Invalid input");
-  } else {
-    const timeString = minutesToTime(minutes);
-    console.log(timeString);
-  }
-  const btn = document.querySelector(".calculator__form--find")
+
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours.toString().padStart(2, '0')} годин ${remainingMinutes.toString().padStart(2, '0')} хв.`;
+}
+
+const minutes = parseInt(prompt("Введіть число хвилин: "));
+if (isNaN(minutes)) {
+  console.log("Invalid input");
+} else {
+  const timeString = minutesToTime(minutes);
+  console.log(timeString);
+}
+const btn = document.querySelector(".calculator__form--find")
+btn.addEventListener("click", minutesToTime)
