fac63cf679ee56cc8401d0a1fad47f6880ebb077
Sonic21354 <85310527+Sonic21312213@users.noreply.github.com>
2024-07-22 18:52:12 +0200

    upgrade

--- src/js/year-check.js
+++ src/js/year-check.js
@@ -3,14 +3,11 @@ function isLeapYear(year) {
 }
 
 function checkYear() {
-    const year = parseInt(document.getElementById('year-input').value);
+    const year = document.getElementById('year-input').value;
     const message = document.getElementById('message');
 
-    if (isNaN(year) || year < 1900) {
+    if ((year) < 1900) {
         message.textContent = 'Будь ласка, введіть рік не менше 1900';
-        message.className = 'message error';
-    } else if (year < 1950) {
-        message.textContent = 'Будь ласка ведіть справжній рік.';
         message.className = 'message error';
     } else if (isLeapYear(year)) {
         message.textContent = 'Ви народилися у високосний рік!';
