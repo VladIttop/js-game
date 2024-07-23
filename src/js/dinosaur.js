b26a80a5309fa13b638b7abc9b67a25293ee74b4
Sasha <129985153+Sasha76543@users.noreply.github.com>
2024-07-23 22:04:30 +0300

    dinosaur

--- src/js/dinosaur.js
+++ src/js/dinosaur.js
@@ -1,6 +1,14 @@
 const container=document.querySelector(".dinosaur__container");
 const dinosaur=document.querySelector(".dinosaur__dino");
-
+const pointsEl = document.querySelector(".dinosaur__points");
+const cactus = document.querySelector(".dinosaur__cactus");
+let x = Math.floor(dinosaur.getBoundingClientRect().x + dinosaur.getBoundingClientRect().width/2)
+let y = Math.floor(dinosaur.getBoundingClientRect().y + dinosaur.getBoundingClientRect().width/2)
+let points = 0
+let timer1;
+let timer2;
+let timer3;
+let isanimation = false;
 const jump = function(){
     dinosaur.classList.add("jump");
 };
