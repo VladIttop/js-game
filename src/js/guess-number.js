e14c7a87852031c063c8b8ed7a72f9ef63d53ef6
Sonic21354 <85310527+Sonic21312213@users.noreply.github.com>
2024-07-24 14:57:08 +0200

    number-text
    text

--- src/js/guess-number.js
+++ src/js/guess-number.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random() * 1000) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 function checkNumber() {
     const number = parseInt(document.getElementById('number-input').value);
@@ -13,10 +13,10 @@ function checkNumber() {
     } else if (number === randomNumber) {
         message.textContent = 'Вітаю, ви вгадали число! ' + number + '!';
         message.className = 'message_numb success';
-        randomNumber = Math.floor(Math.random() * 1000) + 1; 
+        randomNumber = Math.floor(Math.random() * 100) + 1; 
     } else {
         message.textContent = 'Спробуйте ще раз! Число було ' + randomNumber + '!';
         message.className = 'message_numb error';
-        randomNumber = Math.floor(Math.random() * 1000) + 1; 
+        randomNumber = Math.floor(Math.random() * 100) + 1; 
     }
 }
