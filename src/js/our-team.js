190d1ae76d5c723cd099bf66ec78da25dd4bb08a
Sasha <129985153+Sasha76543@users.noreply.github.com>
2024-07-24 08:28:17 +0300

    our-team

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -1,35 +1,25 @@
+const slider = document.querySelector('.slider-list');
+const slides = document.querySelectorAll('.slider-list__slide');
+const prevButton = document.querySelector('.button-previous');
+const nextButton = document.querySelector('.button-next');
+let currentIndex = 0;
 
-    const slider = document.querySelector('.slider-list');
-    const slides = document.querySelectorAll('.slider-list__slide');
-    const prevButton = document.querySelector('.button-previous');
-    const nextButton = document.querySelector('.button-next');
-    let currentIndex = 0;
+function updateSliderPosition() {
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 
-    function updateSliderPosition() {
-        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-
-    function hideSlide(index) {
-        slides[index].style.display = 'none';
-    }
-
-    prevButton.addEventListener('click', () => {
-        if (currentIndex > 0) {
-            hideSlide(currentIndex);
-            currentIndex--;
-            updateSliderPosition();
-        }
-    });
-
-    nextButton.addEventListener('click', () => {
-        if (currentIndex < slides.length - 1) {
-            hideSlide(currentIndex);
-            currentIndex++;
-            updateSliderPosition();
-        } else {
-            hideSlide(currentIndex);
-        }
-    });
-
-
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
     updateSliderPosition();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+    updateSliderPosition();
+  }
+});
+
+updateSliderPosition();
