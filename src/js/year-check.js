ad40ad547e34a58b8b00dd907771188f0db556fb
VladIttop <129985238+VladIttop@users.noreply.github.com>
2024-07-28 13:57:54 +0100

    absolutely done

--- src/js/year-check.js
+++ src/js/year-check.js
@@ -7,9 +7,12 @@ function isLeapYear(year) {
 function checkYear() {
   const year = document.getElementById('year-input').value;
   const message = document.getElementById('message');
-  if (year < 1900) {
+  if (year === '') {
+    message.textContent = 'Будь ласка, введіть рік!';
+    message.classList.remove('success');
+    message.classList.add('error');
+  } else if (year < 1900) {
     message.textContent = 'Будь ласка, введіть рік не менше 1900';
-    message.className = 'message error';
     message.classList.remove('success');
     message.classList.add('error');
   } else if (isLeapYear(year)) {
