0dde7b46526c39a39a8cbcb50b99a024dd93f035
Yulchikss <yuliapelia19@gmail.com>
2024-07-29 13:55:12 +0300

    feat: corrected game

--- src/js/dinosaur.js
+++ src/js/dinosaur.js
@@ -3,6 +3,32 @@ const container = document.querySelector('.dinosaur__container');
 const pointsToShow = document.querySelector('.dinosaur__points');
 const cactus = document.querySelector(".dinosaur__cactus");
 let points = 0;
+
+
+const startGame = () => {
+  points = 0;
+
+  // pointsToShow.textContent = points;
+  cactus.classList.add('cactus__animation');
+
+  setInterval(function () {
+    pointsToShow.textContent = points;
+
+    points++;
+  }, 100);
+
+  console.log('game started');
+
+  setInterval(function () {
+    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 160) {
+      stopGame();
+    }
+  }, 10);
+
+}
 const jump = function () {
   if (dino.classList != 'jump') {
     dino.classList.add('jump');
@@ -12,30 +38,33 @@ const jump = function () {
   }
 };
 
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+
+  if (e.code === 'Space') {
+    startGame();
+  }
+});
 container.addEventListener('click', () => {
+
   jump();
+
 });
 
-setInterval(function () {
-  points++;
-  pointsToShow.textContent = points;
-},100);
 
-let isAlive = setInterval(function() {
-  const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
-  const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
 
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 160) {
-    stopGame();
-  }
-}, 10);
 
 function stopGame() {
-  const gameOverMessage = document.createElement('div');
-  gameOverMessage.className = 'game-over-message';
-  gameOverMessage.textContent = 'Гра Закінчена!';
-  container.appendChild(gameOverMessage);
-  dino.style.animation = 'none';
-  cactus.style.animation = 'none';
+  // const gameOverMessage = document.createElement('div');
+  cactus.classList.remove('cactus__animation');
+
+  // gameOverMessage.className = 'game-over-message';
+  // gameOverMessage.innerHTML = 'Гра Закінчена!';
+  // container.appendChild(gameOverMessage);
+  // dino.style.animation = 'none';
+  // cactus.style.animation = 'none';
   points = 0;
+
+
+
 }
