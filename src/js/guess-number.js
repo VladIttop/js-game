51e4e69a65c052f467764d52f53b31c8450c30d6
VladIttop <129985238+VladIttop@users.noreply.github.com>
2024-07-29 20:02:08 +0100

    done

--- src/js/guess-number.js
+++ src/js/guess-number.js
@@ -1,22 +1,22 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+const btnGuessNumber = document.querySelector(".guess");
 
 function checkNumber() {
-    const number = parseInt(document.getElementById('number-input').value);
-    const message = document.getElementById('message_numb');
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+  const number = parseInt(document.getElementById('number-input').value);
+  const message = document.getElementById('message_numb');
+  if (number === randomNumber) {
+    message.textContent = 'Вітаю, ви вгадали число! ' + number + '!';
+    message.classList.remove('error');
+    message.classList.add('success');
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+  } else {
+    message.textContent = 'Спробуйте ще раз! Число було ' + randomNumber + '!';
+    message.classList.remove('success');
+    message.classList.add('error');
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+  }
+};
 
-    console.log('Загадане число:', randomNumber);
-    console.log('Введене число:', number);
-
-    if (isNaN(number)) {
-        message.textContent = 'Будь ласка, введіть число.';
-        message.className = 'message_numb error';
-    } else if (number === randomNumber) {
-        message.textContent = 'Вітаю, ви вгадали число! ' + number + '!';
-        message.className = 'message_numb success';
-        randomNumber = Math.floor(Math.random() * 100) + 1; 
-    } else {
-        message.textContent = 'Спробуйте ще раз! Число було ' + randomNumber + '!';
-        message.className = 'message_numb error';
-        randomNumber = Math.floor(Math.random() * 100) + 1; 
-    }
-}
+btnGuessNumber.addEventListener("click", () => {
+    checkNumber();
+});
